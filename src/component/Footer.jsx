e327f206f7497319d66585002c083cd8b516a2c6
Yazid Yasykur <yasykury@gmail.com>
2022-07-27 21:00:19 +0700

    Fixed responsive

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -16,7 +16,7 @@ function Footer() {
                 <ArrowCircleUpIcon className="max-w-[2.5rem] transition delay-75 hover:text-cyan-600" />
             </div>
 
-            <div className="text-sm my-6 max-w-xl ">
+            <div className="text-xs my-6 max-w-xl sm:text-sm">
                 <h3 className="font-bold text-[#999999]">Resources</h3>
                 <ul className="grid grid-cols-3 grid-flow-row cursor-pointer">
                     <li className="transition delay-75 hover:text-cyan-600">Support-A-Creator</li>
@@ -32,7 +32,7 @@ function Footer() {
                 </ul>
             </div>
 
-            <div className="text-sm my-6 max-w-xl">
+            <div className="text-xs my-6 max-w-xl sm:text-sm">
                 <h3 className="font-bold text-[#999999]">Made By Epic Games</h3>
                 <ul className="grid grid-cols-2 grid-flow-row cursor-pointer">
                     <li className="transition delay-75 hover:text-cyan-600">Battle Breakers</li>
@@ -49,7 +49,7 @@ function Footer() {
             <p className="text-xs my-6 max-w-xl">© 2022, Epic Games, Inc. All rights reserved. Epic, Epic Games, the Epic Games logo, Fortnite, the Fortnite logo, Unreal, Unreal Engine, the Unreal Engine logo, Unreal Tournament, and the Unreal Tournament logo are trademarks or registered trademarks of Epic Games, Inc. in the United States of America and elsewhere. Other brands or product names are the trademarks of their respective owners. Non-US transactions through Epic Games International, S.à r.l. </p>
 
             
-            <ul className="flex items-center flex-col text-xs md:flex-row gap-4">
+            <ul className="flex items-center flex-col gap-2 text-xs sm:flex-row">
                 <li>Terms of Service</li>
                 <li>Privacy Policy</li>
                 <li>Store Refund Policy</li>
