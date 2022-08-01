3275b7b6ea2fbef91e9e5ee9b519594112b3fb93
zakynaufal98 <zakynaufal1998@gmail.com>
2022-08-01 22:06:06 +0700

    update

--- src/component/Detail.jsx
+++ src/component/Detail.jsx
@@ -8,25 +8,12 @@ import { useState, useEffect } from "react";
 import { listGames } from "../services/GetData";
 
 const DetailGameCard = () => {
-  GetGames();
-
-  const [game, setGames] = useState({});
-
-  let params = useParams();
-
-  console.log(params);
-
-  useEffect(() => {
-    const chosenGame = setGames(params.gamesId);
-    setGames(chosenGame);
-  }, []);
-
   return (
     <div className="text-white">
       <div className="card-container ml-20 mr-20 text-white m-3 flex border-2 p-4 gap-4 justify-between">
         <div className="detail-image w-50 ">
           <img
-            src={`https://www.freetogame.com/g/${game?.id}/thumbnail.jpg`}
+            src={`https://www.freetogame.com/g//thumbnail.jpg`}
             alt=""
             className="object-fill min-w-full "
           />
@@ -34,8 +21,8 @@ const DetailGameCard = () => {
         <div className="description">
           <ul className="flex flex-col gap-2">
             <h1 className="uppercase mb-6"></h1>
-            <li>Platform: {game?.title}</li>
-            <li>Release Date: {game?.re}</li>
+            <li>Platform: </li>
+            <li>Release Date: </li>
             <li>Publisher: </li>
             <li>Developer: </li>
           </ul>
@@ -45,19 +32,37 @@ const DetailGameCard = () => {
   );
 };
 
-export function GetGames(gamesId) {
+const DetailGame = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     listGames().then((data) => setData(data.slice(0, 20)));
   }, []);
-  return data.find((game) => game.id === gamesId);
-}
 
-const DetailGame = () => {
+  console.log(data[0].title);
+
+  function getGames(gamesId) {
+    return data.find((game) => game.id === gamesId);
+  }
+
+  const [game, setGames] = useState([]);
+
+  let params = useParams();
+
+  console.log(params);
+
+  useEffect(() => {
+    const chosenGame = getGames(params.gamesId);
+    setGames(chosenGame);
+  }, []);
+
+  console.log(game?.title);
+
   return (
     <div>
-      <Navbar />;<DetailGameCard />
+      <div></div>
+      <Navbar />;
+      <DetailGameCard />
       <CardCarousel />
       <Footer />
     </div>
