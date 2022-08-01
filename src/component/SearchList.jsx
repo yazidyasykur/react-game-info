0ac41b66075fdc3704f06d080e6975b9e6897846
Yazid Yasykur <yasykury@gmail.com>
2022-08-01 19:42:13 +0700

    update

--- src/component/SearchList.jsx
+++ src/component/SearchList.jsx
@@ -2,7 +2,7 @@
 function SearchList(props){
 
     return(
-        <div className="bg-[#2a2a2a] text-white max-w-sm overflow-clip absolute z-10 translate-x-[160px]">
+        <div className="bg-[#2a2a2a] text-white max-w-sm overflow-clip absolute z-10 translate-x-[40px] md:translate-x-[160px]">
             <ul>
                 {props.item.map(item => <li className="py-1 px-3 hover:bg-[#3a3a3a] cursor-pointer">{item}</li>)}
             </ul>
