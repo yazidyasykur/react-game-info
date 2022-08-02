f6916eaf88ba61bd558ad465d24ad6b4fa08de8d
Yazid Yasykur <yasykury@gmail.com>
2022-08-02 18:41:59 +0700

    fixed details

--- src/component/Detail.jsx
+++ src/component/Detail.jsx
@@ -1,32 +1,29 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CardCarousel from "./Carousel";
-import epic from "../asets/Epic.png";
-import { useLocation } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { listGames } from "../services/GetData";
+import { byCategory, gameDetails } from "../services/GetData";
 
 const DetailGameCard = () => {
-  GetGames();
-
   const [game, setGames] = useState({});
 
-  let params = useParams();
+  let {id} = useParams();
 
-  console.log(params);
+  console.log(id);
 
   useEffect(() => {
-    const chosenGame = setGames(params.gamesId);
-    setGames(chosenGame);
+    gameDetails(id).then((data) => setGames(data));
   }, []);
 
+
+  console.log(game)
   return (
     <div className="text-white">
       <div className="card-container ml-20 mr-20 text-white m-3 flex border-2 p-4 gap-4 justify-between">
         <div className="detail-image w-50 ">
           <img
-            src={`https://www.freetogame.com/g/${game?.id}/thumbnail.jpg`}
+            src={`https://www.freetogame.com/g/${game.id}/thumbnail.jpg`}
             alt=""
             className="object-fill min-w-full "
           />
@@ -34,10 +31,10 @@ const DetailGameCard = () => {
         <div className="description">
           <ul className="flex flex-col gap-2">
             <h1 className="uppercase mb-6"></h1>
-            <li>Platform: {game?.title}</li>
-            <li>Release Date: {game?.re}</li>
-            <li>Publisher: </li>
-            <li>Developer: </li>
+            <li>Platform: {game.title}</li>
+            <li>Release Date: {game.release_date}</li>
+            <li>Publisher: {game.publisher}</li>
+            <li>Developer: {game.developer}</li>
           </ul>
         </div>
       </div>
@@ -45,14 +42,14 @@ const DetailGameCard = () => {
   );
 };
 
-export function GetGames(gamesId) {
-  const [data, setData] = useState([]);
+// export function GetGames(gamesId) {
+//   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    listGames().then((data) => setData(data.slice(0, 20)));
-  }, []);
-  return data.find((game) => game.id === gamesId);
-}
+//   useEffect(() => {
+//     listGames().then((data) => setData(data.slice(0, 20)));
+//   }, []);
+//   return data.find((game) => game.id === gamesId);
+// }
 
 const DetailGame = () => {
   return (
